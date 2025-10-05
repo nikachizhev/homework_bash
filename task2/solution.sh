3f5b025e2d32f71514348ae72f46bcf205b1d5b7
nikachizhev <chizhevskaianika@mail.ru>
2025-10-05 23:05:01 +0300

    исправлено количество и имя

--- task2/solution.sh
+++ task2/solution.sh
@@ -2,7 +2,6 @@
 DIR="$1"
 file_count=$(find "$DIR" -type f | wc -l)
 dir_count=$(find "$DIR" -type d | wc -l)
-dir_count=$((dir_count - 1))
 largest_file=$(find "$DIR" -type f -exec du -b {} + | sort -nr | head -n 1)
 
 echo "Files: $file_count"
@@ -10,4 +9,4 @@ echo "Dirs: $dir_count"
 
 biggest_size=$(echo "$largest_file" | awk '{print $1}')
 biggest_name=$(echo "$largest_file" | cut -d' ' -f2-)
-echo "Largest file: $biggest_name ($biggest_size bytes)"
+echo "Largest file: $(basename "$biggest_name") ($biggest_size bytes)"
