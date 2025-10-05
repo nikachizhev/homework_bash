54e725f86ccdb8c227d326a98e795df260345f23
nikachizhev <chizhevskaianika@mail.ru>
2025-10-05 23:34:40 +0300

    исправила директории

--- task8/solution.sh
+++ task8/solution.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 LOG_DIR="$1"
-ARCHIVE_DIR="$LOG_DIR/archive"
+ARCHIVE_DIR="$2"
 mkdir -p "$ARCHIVE_DIR"
 find "$LOG_DIR" -type f -name "*.log" -mtime +7 | while read logfile; do
  filename=$(basename "$logfile") 
- cp "$logfile" "$ARCHIVE_DIR/$filename"
+ mv "$logfile" "$ARCHIVE_DIR/$filename"
  gzip -f "$ARCHIVE_DIR/$filename"
 done
